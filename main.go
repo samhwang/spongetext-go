bbed34433caf5f529c8ba6d60f16b8f1bbea6112
Sam Huynh <samhwang2112.dev@gmail.com>
2021-01-04 16:23:38 +1100

    Split spongeCLI and getInputs into smaller functions

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/urfave/cli/v2"
 )
 
-func greet(c *cli.Context) error {
-	fmt.Printf("Hello %q", c.Args().Get(0))
-	return nil
-}
-
 func main() {
 	app := &cli.App{
-		Action: greet,
+		Action: spongeCLI,
 	}
 
 	err := app.Run(os.Args)
